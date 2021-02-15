cdb561853734809a960a27a0fc65ff8fb76677c7
Henry Velasquez <hvelas93@gmail.com>
2021-02-15 21:21:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,8 +15,23 @@ values=[]
 
     end
 
-    
+element_index=0
+min_val=""
 
+    while element_index < values.length do
+
+      if element_index==0
+        min_val=values[counter]
+        element_index +=1
+
+      elsif values[element_index]<min_val
+        min_value=values[element_index]
+
+      else element_index+=1
+
+      end
+     x=values.find_index(min_value)
+    end
+
+keys[x]
 end
-
-key_for_min_value(name_hash)
