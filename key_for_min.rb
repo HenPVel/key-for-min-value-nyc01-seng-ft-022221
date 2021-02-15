0673a25c9751be1d577962a2983e32fca375be35
Henry Velasquez <hvelas93@gmail.com>
2021-02-15 21:03:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,15 +5,17 @@ name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
 
+keys=[]
+values=[]
  name_hash.each do |key,value|
 
-puts key
-puts value
-  # if value < min_val
-  #   min_val=value
+keys << key
+values << value
 
-#   end
  end
+
+puts keys
+puts values
 
 end
 
