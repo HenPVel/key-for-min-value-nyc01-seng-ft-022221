d3916bf9673504d7448b97edd116f62f08987b20
Henry Velasquez <hvelas93@gmail.com>
2021-02-15 21:48:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -30,7 +30,6 @@ min_val=""
         min_value=values[element_index]
         element_index+=1
 
-      else element_index+=1
     binding.pry
       end
      key_index=values.find_index(min_value)
